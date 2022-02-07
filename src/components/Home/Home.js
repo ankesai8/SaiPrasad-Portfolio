cf475e2cca5abb06a23fb60afa5563374980e766
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-07 19:22:47 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,7 +29,7 @@ function Home() {
                 <Type />
               </div>
          
-          <Row style={{ justifyContent: "left", position: "relative"}}>
+          <Row style={{ justifyContent: "center", position: "relative"}}>
           <Button variant="primary" href={ResumePdf} target="_blank">
             <AiOutlineDownload />
             &nbsp;Resume
