7e72f1b8a2868d72c526a24f2e6e18415e63c6da
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-02-17 18:07:03 +0530

    Update Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -72,7 +72,7 @@ function Home2() {
           </Col>
         </Row>
          <Row style={{ justifyContent: "center", position: "relative"}}>
-          <Button variant="primary" a href="https://drive.google.com/file/d/1aK-tPdYNqZgjIYcUrA1FZlFIbicAviYS/view?usp=sharing" target="_blank" ><AiOutlineDownload />
+          <Button variant="primary" a href="https://drive.google.com/file/d/1mE_Gb4110lRG9HfQlBPQqND2h7y-hAWf/view?usp=sharing" target="_blank" ><AiOutlineDownload />
             &nbsp;Resume
           </Button>
         </Row>
